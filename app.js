fe55a53f529a0b498c1c4b00e4a0b90ec5fecc17
sakib-git <sakibulhasan32002@gmail.com>
2025-08-27 20:05:46 +0600

    heart js & copy js done

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
+// Heart section
 const heartCount = document.getElementById('heartCount')
 const heart = document.getElementsByClassName('heart')
-
-
 let heartNumber = 0;
 for(let hrt of heart){
   hrt.addEventListener('click', () => {
@@ -12,18 +11,24 @@ for(let hrt of heart){
 }
 
 
-const coin = document.getElementById('coin')
-const callbtn = document.getElementById('callbtn')
 
-callbtn.addEventListener('click', () => {
+// copy section
+const copybtn = document.getElementsByClassName('copybtn');
 
+    let copyCount = 0;
+for (const btn of copybtn) {
+  btn.addEventListener('click', () => {
+    
+    const numbers = btn.parentNode.parentNode.children[2].children[0].innerText;
+
+    const copyCounts = document.getElementById('copyCount')
+    copyCount++;
+    copyCounts.innerText = copyCount
+
+    // console.log(numbers)
+
+    alert(`📋 নম্বর কপি হয়েছে: ${numbers}`);
+    
  
-  let coinvalue = Number(coin.innerText);
-  if(coinvalue < 20){
-    return
-  }
-  const result = coinvalue - 20;
-  coin.innerText = result;
-
-
-})
+  });
+}
